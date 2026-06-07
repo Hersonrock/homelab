a46e24278ad7a0e6a0e79d385086f5d00e7c6422
Spike <hjpg20@gmail.com>
2026-06-07 14:14:31 -0500

    Adds forwarding config script

---
+++ labs/sm-sysctl-mitm-fe25c281/ubu-vm.lab/disable_forwardconfig.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -z "$SUDO_USER" ]; then
+	echo "Use Sudo"
+	exit 1
+fi
+
+#Enable kernel parameter for forwarding
+sysctl -w net.ipv4.ip_forward=0 > /dev/null
+sysctl -p
+echo "Kernel parameter enabled"
+
+#enable firewall forward
+iptables -P  FORWARD DROP 
+echo "iptable updated"
